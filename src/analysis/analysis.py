2d8777c638567fce76d5bcbd052102035107b1f1
kashyap310 <kshyp310@gmail.com>
2024-01-11 10:01:59 +0530

    new

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -17,6 +17,7 @@ def analyze(url, html_content):
         'usernames': usernames,
         'links': links
     }
+    #data
     print("work")
     insert_query = "INSERT INTO analytic_table (url, email, username, links) VALUES (%s, %s, %s, %s)"
     execute_query(insert_query, (data['url'], data['emails'], data['usernames'], data['links']))
